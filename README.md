b56b9b2caf08d55ccb806ce869687a1800f5c7fb
DeShawn Jackson <shawnynicole@gmail.com>
2021-07-15 16:02:40 -0500

    Initial working copy

--- README.md
+++ README.md
@@ -1,3 +1,122 @@
 # Print
 
-A description of this package.
+**Print** makes ease of debugging by printing pertinent information to the console.
+
+<p align="center">
+    <a href="#requirements">Requirements</a> • <a href="#installation">Installation</a> • <a href="#usage">Usage</a> • <a href="#author">Author</a> • <a href="#license-mit">License</a>
+</p>
+
+## Requirements
+
+- Xcode 10.2+
+- Swift 5.0+
+
+## Installation
+
+#### Swift Package Manager
+You can use [The Swift Package Manager](https://swift.org/package-manager) to install `Print` by adding the proper description to your `Package.swift` file:
+```swift
+import PackageDescription
+
+let package = Package(
+    name: "YOUR_PROJECT_NAME",
+    targets: [],
+    dependencies: [
+        .package(url: "https://github.com/shawnynicole/Print.git", from: "1.0.0")
+    ]
+)
+```
+For more information on [Swift Package Manager](https://swift.org/package-manager), checkout its [GitHub Page](https://github.com/apple/swift-package-manager).
+
+#### Manually
+
+[Download](https://github.com/shawnynicole/Print/archive/master.zip) the project and copy the `Print` folder into your project to use it in.
+
+## Usage
+
+```swift
+import SwiftUI
+import Print
+
+struct ContentView: View, Print {
+    
+    var body: some View {
+        
+        Text("Hello, world!")
+            .onAppear {
+                
+                // Common usage
+                
+                print("Hello World!")
+                
+                // Custom title
+                // A separator with a custom title is drawn around the text in the console.
+                
+                print(type: .title("Custom"), "Hello World!")
+                
+                // Static
+                
+                MyStruct.sayHello()
+                
+                // MyStruct conforms to Print
+                
+                MyStruct().sayHello()
+                
+                // Handling errors
+                // A separator is drawn around the error in the console so that errors stand out.
+                
+                do {
+                    try getError()
+                } catch {
+                    print(error)
+                }
+                
+                // Superclass
+                
+                AClass().sayHello()
+                
+                // Subclass
+                // Subclass calls super. Notice Print.AClass.swift 15 and Print.BClass.swift 16 is printed to the console.
+                
+                BClass().sayHello()
+            }
+    }
+    
+    func getError() throws {
+        throw MyError("This is my error message.")
+    }
+}
+```
+
+```swift
+
+
+[2021-07-15 03:56:44 PM ContentView.swift ContentView.body 25] Hello World!
+
+************************************************************** CUSTOM **************************************************************
+[2021-07-15 03:56:44 PM ContentView.swift ContentView.body 30] Hello World!
+***********************************************************************************************************************************
+
+[2021-07-15 03:56:44 PM MyStruct.swift MyStruct.sayHello() 15] Hello, World!
+
+[2021-07-15 03:56:44 PM MyStruct.swift MyStruct.sayHello() 19] Hello, World!
+
+************************************************************** ERROR **************************************************************
+[2021-07-15 03:56:44 PM ContentView.swift ContentView.body 46] MyError(message: "This is my error message.")
+***********************************************************************************************************************************
+
+[2021-07-15 03:56:44 PM AClass.swift AClass.sayHello() 15] Hello, AClass!
+
+[2021-07-15 03:56:44 PM AClass.swift BClass.sayHello() 15] Hello, AClass!
+
+[2021-07-15 03:56:44 PM BClass.swift BClass.sayHello() 16] Hello, BClass!
+
+```
+
+## Author
+
+shawnynicole
+
+## License
+
+Print is available under the MIT license. See the LICENSE file for more info.
--- Sources/Print/Print.swift
+++ Sources/Print/Print.swift
@@ -1,3 +1,154 @@
-struct Print {
-    var text = "Hello, World!"
+//
+//  Print.swift
+//
+//
+//  Created by DeShawn Jackson on 11/18/19.
+//
+
+import Foundation
+
+// MARK: -
+
+public enum PrintType: Equatable { //String {
+    case standard, error, warning, fatal, title(String?)
+    
+    public var value: String {
+        
+        let value: String = {
+            switch self {
+            case .standard: return "standard"
+            case .error: return "error"
+            case .warning: return "warning"
+            case .fatal: return "fatal"
+            case .title(let x): return x ?? ""
+            }
+        }()
+        
+        return value
+    }
+    
+    public static func ==(lhs: PrintType, rhs: PrintType) -> Bool {
+        
+        switch (lhs, rhs) {
+        case (.standard, .standard): return true
+        case (.error, .error): return true
+        case (.warning, .warning): return true
+        case (.fatal, .fatal): return true
+        case (.title(_), .title(_)): return true
+        default: return false
+        }
+    }
+}
+
+// MARK: -
+
+extension NSObject: Print { }
+extension String: Print { }
+extension Array: Print { }
+
+// MARK: -
+
+public protocol Print { }
+
+extension Print {
+    
+    // MARK: -
+    
+    public func print(type printType: PrintType = .standard, _ items: Any?..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function:  Any = #function) {
+
+        var printType = printType
+        
+        if printType == .standard && items.count == 1 && items.first is Error {
+            printType = .error
+        }
+        
+        type(of: self).print(type: printType, items: items, separator, terminator, file, line, function)
+    }
+    
+    public static func print(type printType: PrintType = .standard, _ items: Any?..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function:  Any = #function) {
+        
+        print(type: printType, items: items, separator, terminator, file, line, function)
+    }
+    
+//    // MARK: -
+//
+//    public func print(error: Error, title: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function:  Any = #function) {
+//
+//        type(of: self).print(type: .error, items: [error], separator, terminator, file, line, function)
+//    }
+
+    // MARK: -
+    
+    private static func print(type printType: PrintType, items: [Any?], _ separator: String = " ", _ terminator: String = "\n", _ file: String = #file, _ line: Int = #line, _ function:  Any = #function) {
+        
+        // Format Date
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd hh:mm:ss a"
+        let now = formatter.string(from: Date())
+        //let now = Date()
+        //let date = now.format(dateStyle: .short, timeStyle: .none)
+        //let time = now.format(dateStyle: .none, timeStyle: .medium)
+        
+        // Get file name
+        
+        let fileURL = URL(fileURLWithPath: file)
+        let bundle: String = {
+            if let bundle = Bundle(url: fileURL.deletingLastPathComponent())?.bundleURL.lastPathComponent {
+                return bundle + "."
+            } else { return "" }
+        }()
+        let file = fileURL.lastPathComponent
+        
+        // Create separators
+        
+        let before: String = {
+            
+            let value = "\n************************************************************** \(printType.value.uppercased()) **************************************************************"
+            
+            switch printType {
+            case .standard: return ""
+            // case .title(let string): return string == nil ? "" : value
+            default: return value
+            }
+            
+        }()
+        
+        let after: String = {
+            
+            let value = "\n***********************************************************************************************************************************"
+            
+            switch printType {
+            case .standard: return ""
+            // case .title(let string): return string == nil ? value : ""
+            default: return value
+            }
+            
+        }()
+        
+        // Unwrap nil values
+        
+        let items: [Any] = items.map({ $0 == nil ? $0 as Any : $0! })
+        
+        // Print to console
+        
+        Swift.print("\(before)\n[" + [now, "\(bundle)\(file)", "\(self).\(function)", line].joined(" ") + "] ", items.joined(separator) + after, separator: "", terminator: terminator)
+        //if type == .fatal { fatalError() }
+    }
+}
+
+extension Array {
+        
+    public func joined(_ separator: String) -> String {
+        
+        var string = ""
+        
+        self.enumerated().forEach({ (i, element) in
+            if i > 0 { string += separator }
+            string += String(describing: element)
+            
+        })
+        
+        return string
+    }
 }
